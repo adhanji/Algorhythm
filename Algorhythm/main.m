7a7cbef20ad36ba41c09fe3712bfa9dbfd942882
Aleem Dhanji <dhanji.aleem@gmail.com>
2015-01-10 10:07:58 -0500

    Initial Commit

---
+++ Algorhythm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Algorhythm
+//
+//  Created by Aleem Dhanji on 01-10-15.
+//  Copyright (c) 2015 Aleem Dhanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
